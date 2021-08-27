ed0903c2136cbe31a94875841349d9055f56c465
Omar Rizwan <omar.rizwan@gmail.com>
2021-08-26 19:39:34 -0700

    Set up timerfd. Sleeps (blocks) inline; doesn't use scheduler yet.

--- little-epoll-fiber-stuff.lua
+++ little-epoll-fiber-stuff.lua
@@ -14,6 +14,23 @@ struct epoll_event {
 int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event);
 int epoll_wait(int epfd, struct epoll_event *events,
                int maxevents, int timeout);
+
+typedef long int time_t;
+struct timespec {
+    time_t tv_sec;                /* Seconds */
+    long   tv_nsec;               /* Nanoseconds */
+};
+struct itimerspec {
+    struct timespec it_interval;  /* Interval for periodic timer */
+    struct timespec it_value;     /* Initial expiration */
+};
+static const int CLOCK_MONOTONIC = 1;
+int timerfd_create(int clockid, int flags);
+int timerfd_settime(int fd, int flags,
+                    const struct itimerspec *new_value,
+                    struct itimerspec *old_value);
+
+ssize_t read(int fd, void *buf, size_t count);
 ]]
 
 local function cassert(cond)
@@ -39,35 +56,51 @@ fiber.await = function(fd)
    ffi.C.epoll_ctl(fiber._epfd, ffi.C.EPOLL_CTL_ADD, fd, ev)
 
    -- TODO: yield
+   coroutine.yield()
 end
 fiber.sleep = function(us)
    -- FIXME: it's just await on a timerfd
-   coroutine.yield()
+   local fd
+   fiber.await(fd)
 end
 
 fiber.runloop = function()
    fiber._epfd = ffi.C.epoll_create1(0)
    cassert(fiber._epfd ~= -1)
    while true do
-      local numevents = ffi.C.epoll_wait(fiber._epfd, events, 1024, 50)
+      for _, co in ipairs(fiber._fibers_to_resume) do
+         coroutine.resume(co)
+      end
+      fiber._fibers_to_resume = {}
+
+      local numevents = ffi.C.epoll_wait(fiber._epfd, events, 1024, 500)
       for i = 0, numevents - 1 do
          local ev = events[i]
          table.insert(fiber._fibers_to_resume, fiber._fibers[ev.data.u32])
       end
       
-      for _, co in ipairs(fiber._fibers_to_resume) do
-         coroutine.resume(co)
-      end
-      fiber._fibers_to_resume = {}
    end
 end
 
 -- Usage:
 
 fiber.dispatch(function()
+      print('hi')
+      
+      local timerfd = ffi.C.timerfd_create(ffi.C.CLOCK_MONOTONIC, 0)
+      local function sleep(ns)
+         local spec = ffi.new('struct itimerspec', {it_value={tv_nsec=ns}})
+         ffi.C.timerfd_settime(timerfd, 0, spec, nil)
+         local buf = ffi.new('uint64_t[1]')
+         cassert(ffi.C.read(timerfd, buf, ffi.sizeof(buf)) > 0)
+      end
+
       print('hello')
-      fiber.sleep(500000)
+
+      sleep(500000000)
+
       print('bye!')
 end)
 
 fiber.runloop()
+
